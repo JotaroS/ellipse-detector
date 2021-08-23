9319ee644ae106332a170dcf897fb53ffc6c558d
Jotaro <jojojoe77@gmail.com>
2021-08-23 23:45:48 +0200

    brushed up train.py

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
-import numpy as np
+import numpy  as np
 import pandas as pd
 import glob
 import json
 
-from network import EllipseNet
-from sklearn.model_selection import train_test_split
+from network                              import EllipseNet
+from sklearn.model_selection              import train_test_split
 from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
 
 
@@ -36,15 +36,18 @@ def generate_dataset():
         Y.append(y)
     Y = np.asarray(Y)
     Y = Y.astype('float32')
+
     return X, Y
 
 
 def train():
-    X,Y = generate_dataset()
+    X,Y   = generate_dataset()
     model = EllipseNet()
     model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics='mean_squared_error')
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=111)
+
     history = model.fit(X_train, y_train, batch_size=16, epochs=100, validation_data = (X_test, y_test))
+    
     pass
 
 def main():
