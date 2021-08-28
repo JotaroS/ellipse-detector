8e104d09fd4f30a044ec2703d5c03d8c1d4537bb
Jotaro <jojojoe77@gmail.com>
2021-08-28 21:38:41 +0200

    Update train.py

--- train.py
+++ train.py
@@ -3,22 +3,22 @@ import glob
 import json
 
 from dataset                              import EllipseDataset
-from network                              import EllipseNet
+from network                              import EllipseNet, EllipseResNet
 from sklearn.model_selection              import train_test_split
 from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
 
 
 def train():
     dataset = EllipseDataset()
-    X, Y    = dataset.load_dataset(True)
+    X, Y    = dataset.load_dataset(DEBUG=False)
 
-    model   = EllipseNet()
+    model   = EllipseResNet()
     model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics='mean_squared_error')
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=111)
 
     history = model.fit(X_train, y_train, batch_size=32, epochs=10, validation_data = (X_test, y_test))
 
-    model.save('my_model_sml', save_format='tf')
+    model.save('my_model_resnet', save_format='tf')
     pass
 
 def main():
